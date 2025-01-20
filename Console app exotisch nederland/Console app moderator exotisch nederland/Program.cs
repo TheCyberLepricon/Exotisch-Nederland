92ca4ccb1b9963edb021c1b95cf7584a210e8601
TheCyberLepricon <TheCyberLepricon@users.noreply.github.com>
2025-01-20 15:34:19 +0100

    Console app moderator exotischy nederland toegevoegd

---
+++ Console app exotisch nederland/Console app moderator exotisch nederland/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Console_app_moderator_exotisch_nederland
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
